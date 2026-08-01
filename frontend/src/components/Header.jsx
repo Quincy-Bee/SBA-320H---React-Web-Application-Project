d89113af77d6c0fb98015344000d9c25e6d5ff24
Quincy-Bee <quincybee@icloud.com>
2026-08-01 19:13:52 -0400

    connected to app.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Movie Watchlist App</h1>
+            <p>serch for movies and save to watchlist</p> 
+        </header>
+    );
+}
+
+export default Header;
