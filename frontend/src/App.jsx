027684beb3309aff5cad335e248c1c3f3a60cc7e
Quincy-Bee <quincybee@icloud.com>
2026-08-01 20:17:14 -0400

    local storage added with use effect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
@@ -10,6 +10,44 @@ function App() {
 
   // State for WatchList
   const [watchlist, setWatchlist] = useState([]);
+
+  // use effect for local storage 1
+  useEffect(() => {
+
+  const savedMovies = localStorage.getItem("watchlist");
+
+  if (savedMovies) {
+    setWatchlist(JSON.parse(savedMovies));
+  }
+
+}, []);
+
+  // use effect for local storage 2
+
+useEffect(() => {
+
+  localStorage.setItem(
+    "watchlist",
+    JSON.stringify(watchlist)
+  );
+
+}, [watchlist]);
+
+  // end use effect for local storage
+
+  // remove movie
+
+  const removeFromWatchlist = (id) => {
+
+  const updatedWatchlist = watchlist.filter(
+    (movie) => movie.imdbID !== id
+  );
+
+  setWatchlist(updatedWatchlist);
+
+};
+ 
+// end remove movie
 
   return (
     <div>
